473fb26bd24d5da9d6bcd47347d4fc4ee9846e01
zeapherine <zeapherine@gmail.com>
2022-04-02 23:19:08 +0530

    tailwind configuration

--- app/root.jsx
+++ app/root.jsx
@@ -1,33 +1,77 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "remix";
+import { Outlet, LiveReload, Link, Links, Meta } from 'remix';
+import styles from './styles/app.css';
 
-export function meta() {
-  return {
-    charset: "utf-8",
-    title: "New Remix App",
-    viewport: "width=device-width,initial-scale=1",
-  };
+export function links() {
+	return [{ rel: 'stylesheet', href: styles }];
 }
 
+export const meta = () => {
+	const description = 'A blog about remix';
+	const keyword = 'remix, react, javascript';
+
+	return {
+		description,
+		keyword,
+	};
+};
+
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<Document>
+			<Layout>
+				<Outlet />
+			</Layout>
+		</Document>
+	);
+}
+
+function Document({ children, title }) {
+	return (
+		<html lang='en'>
+			<head>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<Meta />
+				<Links />
+				<title>{title ? title : 'Remix Blog'}</title>
+			</head>
+			<body>
+				{children}
+				{process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+			</body>
+		</html>
+	);
+}
+
+function Layout({ children }) {
+	return (
+		<>
+			<nav className='navbar'>
+				<Link to='/' className='logo'>
+					Remix
+				</Link>
+
+				<ul className='nav'>
+					<li>
+						<Link to='/posts'>Posts</Link>
+					</li>
+				</ul>
+			</nav>
+
+			<div className='container'>{children}</div>
+		</>
+	);
+}
+
+export function ErrorBoundary({ error }) {
+	console.log(error);
+
+	return (
+		<Document>
+			<Layout>
+				<h1>Error</h1>
+				<p>{error.message}</p>
+			</Layout>
+		</Document>
+	);
 }
